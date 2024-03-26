f23282ae304cfe300ae14005853c50ba9de071c5
Joshua Calafell <joshcalafell@gmail.com>
2024-03-26 01:30:09 -0700

    Added wrap layout and buttons with alerts

--- apps/pitch-nativescript/src/features/home/components/home.component.ts
+++ apps/pitch-nativescript/src/features/home/components/home.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { setStatusBarColor } from '../../../utils';
+import { EventData, Frame, NavigatedData, Page } from '@nativescript/core';
+
+const items = Array.from({ length: 100 }).map((_, i) => ({
+  title: `Item ${i}`,
+}));
 
 @Component({
   moduleId: module.id,
@@ -15,6 +20,49 @@ export class HomeComponent {
   ];
 
   ngOnInit() {
-    setStatusBarColor('dark', '#97d9e9');
+    setStatusBarColor('dark', '#333');
+  }
+
+  onLogin() {
+    alert('Logging in');
+  }
+
+  onRegister() {
+    alert('Registering');
+  }
+
+  goToDetails() {
+    alert('Going to details');
+  }
+
+  onLoaded(args: EventData) {
+    const page = args.object as Page;
+  }
+
+  onUnloaded(args: EventData) {
+    const page = args.object as Page;
+  }
+
+  onLayoutChanged(args: EventData) {
+    const page = args.object as Page;
+  }
+
+  onNavigatedTo(args: NavigatedData) {
+    const page = args.object as Page;
+    console.log(args.isBackNavigation);
+  }
+
+  onNavigatingFrom(args: NavigatedData) {
+    const page = args.object as Page;
+    console.log(args.isBackNavigation);
+  }
+
+  onNavigatedFrom(args: NavigatedData) {
+    const page = args.object as Page;
+    console.log(args.isBackNavigation);
+  }
+
+  onNavigateToList() {
+    console.log('Navigating to list');
   }
 }
