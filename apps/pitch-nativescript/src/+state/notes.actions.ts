9a970b52bf6382349484629b29e9c5e5c5fff85a
Joshua Calafell <joshcalafell@gmail.com>
2024-03-28 17:54:18 -0700

    Notes NgRx setup wip

---
+++ apps/pitch-nativescript/src/+state/notes.actions.ts
@@ -0,0 +1,15 @@
+import { createAction, props } from '@ngrx/store';
+import { NotesEntity } from './notes.models';
+
+export const initNotes = createAction('[Notes Page] Init');
+
+export const loadNotesSuccess = createAction(
+  '[Notes/API] Load Notes Success',
+  props<{ notes: NotesEntity[] }>()
+);
+
+export const loadNotesFailure = createAction(
+  '[Notes/API] Load Notes Failure',
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  props<{ error: any }>()
+);
--- apps/pitch-nativescript/src/app.module.ts
+++ apps/pitch-nativescript/src/app.module.ts
@@ -3,9 +3,19 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './features/shared/shared.module';
 import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import * as fromNotes from './+state/notes.reducer';
+import { NotesEffects } from './+state/notes.effects';
 
 @NgModule({
-  imports: [CoreModule, SharedModule, AppRoutingModule],
+  imports: [
+    CoreModule,
+    SharedModule,
+    AppRoutingModule,
+    StoreModule.forFeature(fromNotes.NOTES_FEATURE_KEY, fromNotes.notesReducer),
+    EffectsModule.forFeature([NotesEffects]),
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
   schemas: [NO_ERRORS_SCHEMA],
